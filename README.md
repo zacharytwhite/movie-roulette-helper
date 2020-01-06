44f129c6c270861da644e60889d21b4aca561a53
Zachary White <zacwhitesmail@gmail.com>
2020-01-06 16:47:41 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# "Random Movie Roulette" Helper
+
+This program is meant to assist in choosing and/or locating movies in Tobias Andersen's Letterboxd list/game, "Random Movie Roulette." Follow [this link](https://letterboxd.com/tobiasandersen2/list/random-movie-roulette/) to view the list and to read his original descripton of it.
+
+## How to Work the Helper
+
+1. Click the "run" button at the top of the page
+2. When prompted for input, key in your choice (1, 2, or 3) and then Enter
+3. Copy/paste the generated URL
+4. Once at the correct page, type Ctrl+F (Windows) or Cmd+F (Mac) and then type in the movie's number to locate it on the page.
+
+### And That's It
+
+I hope this tool is helpful to you. Any feedback would be appreciated and can (ideally) be left as a comment on [my Letterboxd clone of this list](https://letterboxd.com/zachary_white/list/random-movie-roulette-with-number-locator/).
---
+++ main.js
@@ -0,0 +1,60 @@
+// ***** Important Note *****
+// ON PAGE 37 — two numbers, 3618 and 3619 (the "two baddies"), are missing,
+// causing what I'm henceforth going to refer to as the "weird jump."
+// This "weird jump" has been taken into account.
+//
+// Also, "L-list" refers to the Letterboxd.com list this program is for.
+
+MAX = 7552; // Number of movies currently in L-list
+
+
+// Returns a random number between min and max (both inclusive)
+function getRandInt(min, max) {
+  return Math.floor(Math.random() * (max - min+1) ) + min;
+}
+
+
+// Returns link to L-list page containing a particular movie number 
+function getListLink(num) {
+  var page_num; // L-list page to be linked to
+
+  // Before the "weird-jump"
+  if (num < 3701) {
+    if (num % 100 == 0) {
+      page_num = Math.trunc(num/100);
+    } else {
+      page_num = Math.trunc(num/100) + 1;
+    }
+  }
+
+  // After the "weird jump"
+  if (num >= 3701) {
+    if ((num % 100 == 0) || (num % 100 == 1) || (num % 100 == 2)) {
+      page_num = Math.trunc(num/100);
+    } else {
+      page_num = Math.trunc(num/100) + 1;
+    }
+  }
+
+  return "https://letterboxd.com/tobiasandersen2/list/random-movie-roulette/page/" + page_num
+}
+
+
+function generateNumber() {
+  var movie_num = getRandInt(1, MAX);
+  
+  // Generates a new number if either of the "two baddies" are generated
+  (movie_num == 3618 || movie_num == 3619) ? generateNumber() : false;
+
+  return movie_num
+}
+
+
+function addMovie() {
+  var movie_num = generateNumber()
+}
+
+
+function deleteMovie() {
+
+}
